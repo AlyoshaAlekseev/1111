527df44a777f7f71c9f2aa0c61b528b92af56434
ZigMund F <guppy25@mail.ru>
2022-02-02 22:55:02 +0500

    CollectInt

---
+++ src/Collections/Goods.java
@@ -0,0 +1,46 @@
+package Collections;
+public class Goods {
+
+    private String name;
+    private double weight;
+    private String dateOfManufact;
+
+    public Goods(String name, double weight, String dateOfManufact) {
+        this.name = name;
+        this.weight = weight;
+        this.dateOfManufact = dateOfManufact;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String getDateOfManufact() {
+        return dateOfManufact;
+    }
+
+    public void setDateOfManufact(String dateOfManufact) {
+        this.dateOfManufact = dateOfManufact;
+    }
+
+    @Override
+    public String toString() {
+        return "Goods{" +
+                "name='" + name + '\'' +
+                ", weight=" + weight +
+                ", dateOfManufact='" + dateOfManufact + '\'' +
+                '}';
+    }
+}
